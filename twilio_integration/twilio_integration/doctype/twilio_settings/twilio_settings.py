e8057ffcdf705de0df291240d3ea1b9e6e048ab7
RAVIBHARATHI P C <131471282+ravibharathi656@users.noreply.github.com>
2025-02-26 19:26:45 +0530

    chore: update Twilio Settings to use db.set_single_value

--- twilio_integration/twilio_integration/doctype/twilio_settings/twilio_settings.py
+++ twilio_integration/twilio_integration/doctype/twilio_settings/twilio_settings.py
@@ -49,7 +49,7 @@ class TwilioSettings(Document):
 		new_key = self.create_api_key(twilio)
 		self.api_key = new_key.sid
 		self.api_secret = new_key.secret
-		frappe.db.set_value('Twilio Settings', 'Twilio Settings', {
+		frappe.db.set_single_value('Twilio Settings', {
 			'api_key': self.api_key,
 			'api_secret': self.api_secret
 		})
@@ -59,7 +59,7 @@ class TwilioSettings(Document):
 		"""
 		credentials = self.get_application(twilio) or self.create_application(twilio)
 		self.twiml_sid = credentials.sid
-		frappe.db.set_value('Twilio Settings', 'Twilio Settings', 'twiml_sid', self.twiml_sid)
+		frappe.db.set_single_value('Twilio Settings', 'twiml_sid', self.twiml_sid)
 
 	def create_api_key(self, twilio):
 		"""Create API keys in twilio account.
